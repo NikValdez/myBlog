04492fcd3a3781b4fb1ab17de4ba83f331171489
Nik <nerdvestor@gmail.com>
2019-03-07 08:22:28 -0800

    add social icons page

---
+++ src/pages/socialIcons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SocialIcons from '../components/socialIcons'
+
+const Icons = () => (
+  <Layout>
+    <SocialIcons />
+  </Layout>
+)
+
+export default Icons
